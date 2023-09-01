6f26590d065074fd4e060e7997c5e9292da60e88
Julieta-Sanguedolce <julieta.sanguedolce@gmail.com>
2023-09-01 17:57:32 +0100

    added delete functionality

--- src/components/Customer-page/PatientsTable.tsx
+++ src/components/Customer-page/PatientsTable.tsx
@@ -10,6 +10,8 @@ import {
     Th,
     Td,
     TableContainer,
+    Button,
+    ButtonGroup,
 } from "@chakra-ui/react";
 
 export function PatientsTable(): JSX.Element {
@@ -23,7 +25,11 @@ export function PatientsTable(): JSX.Element {
     console.log(listOfPatients);
     useEffect(() => {
         fetchPatientList();
-    }, []);
+    }, [listOfPatients]);
+
+    async function handleDelete(id: number) {
+        axios.delete(baseURL + `/customers/${id}`);
+    }
 
     return (
         <TableContainer>
@@ -39,6 +45,7 @@ export function PatientsTable(): JSX.Element {
                         <Th>Country</Th>
                         <Th>Date of Birth</Th>
                         <Th>Last date of visit</Th>
+                        <Th textAlign="center">Manage</Th>
                     </Tr>
                 </Thead>
                 <Tbody>
@@ -54,6 +61,18 @@ export function PatientsTable(): JSX.Element {
                             <Td>{patient.date_of_birth.substring(0, 10)}</Td>
                             <Td>
                                 {patient.last_date_of_visit.substring(0, 10)}
+                            </Td>
+                            <Td>
+                                <ButtonGroup>
+                                    <Button>✏️</Button>
+                                    <Button
+                                        onClick={() =>
+                                            handleDelete(patient.client_id)
+                                        }
+                                    >
+                                        🗑️
+                                    </Button>
+                                </ButtonGroup>
                             </Td>
                         </Tr>
                     ))}
